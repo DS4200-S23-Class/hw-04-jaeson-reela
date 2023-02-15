870f266db6c74eb4c85d7cc2e8ee703cc331dc1b
jpyeon <jpyeon1216@gmail.com>
2023-02-15 14:46:48 -0500

    changes

--- external.js
+++ external.js
@@ -1,1 +1,17 @@
 //js file
+
+
+// show border of point
+
+function clickBorder() {
+	console.log("Hello");
+}
+
+// store last clicked point 
+
+const lastClick = document.getElementById("last-click");
+
+// add a new point
+function addPoint() {
+	console.log("Hello");
+}
